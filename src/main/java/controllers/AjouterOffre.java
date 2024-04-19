015923c5f5ce4120175a17755080fd000488616e
chaimaattyaoui <153545077+chaimaattyaoui@users.noreply.github.com>
2024-04-19 13:52:48 +0100

    try_4s2

--- src/main/java/controllers/AjouterOffre.java
+++ src/main/java/controllers/AjouterOffre.java
@@ -4,10 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import tn.esprit.models.Offres;
 import tn.esprit.services.OffresService;
 
@@ -32,40 +30,55 @@ public class AjouterOffre {
     @FXML
     private TextField lieuTFO;
 
-
     @FXML
     private TextField imageTFO;
 
     @FXML
     private TextField dateTFO;
+
     private final OffresService os = new OffresService();
 
     @FXML
     void AjouterO(ActionEvent event) {
-
         try {
-            os.add(new Offres(titleTFO.getText(), descriptionTFO.getText(),Boolean.parseBoolean(publishedTFO.getText()),Double.parseDouble(prixTFO.getText()),lieuTFO.getText(),imageTFO.getText(), Date.valueOf(dateTFO.getText())));
+            // Vérifier si la dateTFO est vide
+            if (dateTFO.getText().isEmpty()) {
+                throw new IllegalArgumentException("La date est vide.");
+            }
+
+            // Convertir la chaîne de texte en java.sql.Date
+            Date date = Date.valueOf(dateTFO.getText());
+
+            // Ajouter l'offre avec la date convertie
+            os.add(new Offres(
+                    titleTFO.getText(),
+                    descriptionTFO.getText(),
+                    Boolean.parseBoolean(publishedTFO.getText()),
+                    Double.parseDouble(prixTFO.getText()),
+                    lieuTFO.getText(),
+                    imageTFO.getText(),
+                    date
+            ));
         } catch (SQLException e) {
-           Alert alert = new Alert(Alert.AlertType.ERROR);
-           alert.setTitle("error");
-           alert.setContentText(e.getMessage());
-           alert.showAndWait();
-
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erreur");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+        } catch (IllegalArgumentException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erreur");
+            alert.setContentText("Format de date invalide.");
+            alert.showAndWait();
         }
-
     }
 
     @FXML
     void naviguer(ActionEvent event) {
         try {
-            Parent root= FXMLLoader.load(getClass().getResource("/AfficherOffre.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/AfficherOffre.fxml"));
             titleTFO.getScene().setRoot(root);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
     }
-
-
 }

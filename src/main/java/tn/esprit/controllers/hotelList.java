7081cfb2dab3d269d776c1efa9d36ce86a8663d4
malekbensdira <100678862+malekbensdira@users.noreply.github.com>
2024-05-01 14:04:31 +0100

    01/05/2024 fixed recherche trie stat
    01/05/2024 fixed recherche trie stat

--- src/main/java/tn/esprit/controllers/hotelList.java
+++ src/main/java/tn/esprit/controllers/hotelList.java
@@ -1,151 +1,83 @@
 package tn.esprit.controllers;
 
+import com.sun.javafx.charts.Legend;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.*;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import tn.esprit.models.hotel;
 import tn.esprit.services.Hotelservices;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class hotelList {
 
+    public TextField rechercherParNom;
     @FXML
     private ListView<hotel> hotelListView;
 
     @FXML
-    private Label nomid;
+    private Label nomLabel;
 
     @FXML
-    private Label nbretoilesid;
+    private Label nbretoileLabel;
 
     @FXML
-    private Label emplacementid;
+    private Label emplacementLabel;
 
     @FXML
-    private Label avisid;
-
+    private Label avisLabel;
+    private boolean triAscendant = true;
     @FXML
-    private TextField rechercheTextField; // Champ de recherche
-
-    private Hotelservices hotelService;
-
-    private hotel selectedHotel;
-
-    private boolean triAscendant = true; // Flag pour le sens du tri
-
-    public hotelList() {
-        hotelService = new Hotelservices();
+    private TextField rechercheTextField;
+    @FXML
+    private TextField setupRecherche;
+    @FXML
+    private void afficherStatistiques(ActionEvent actionEvent) {
+        calculerEtAfficherStatistiques();
     }
 
-    @FXML
+    private Hotelservices hotelService = new Hotelservices();
+    private ObservableList<hotel> hotels = FXCollections.observableArrayList();
+
     public void initialize() {
         // Charger les hôtels depuis le service
-        List<hotel> hotels = hotelService.getAll();
+        hotels.addAll(hotelService.getAll());
 
-        // Ajouter les hôtels à la liste de vue
-        hotelListView.getItems().addAll(hotels);
+        // Tri par nombre d'étoiles
 
-        // Personnaliser l'affichage des cellules de la liste
-        hotelListView.setCellFactory(param -> new ListCell<hotel>() {
-            @Override
-            protected void updateItem(hotel item, boolean empty) {
-                super.updateItem(item, empty);
-
-                if (empty || item == null) {
-                    setText(null);
-                    setGraphic(null);
-                } else {
-                    setText(null);
-
-                    // Créer une HBox pour organiser les éléments horizontalement avec de l'espace entre eux
-                    HBox container = new HBox();
-                    container.setSpacing(300); // Ajouter de l'espace horizontal entre les éléments
-
-                    // Créer une VBox pour organiser les éléments verticalement avec de l'espace entre eux
-                    VBox infoContainer = new VBox();
-                    infoContainer.setSpacing(5); // Ajouter de l'espace vertical entre les éléments
-
-                    Label nomLabel = new Label("Nom: " + item.getNom());
-                    Label nbreEtoilesLabel = new Label("Nombre d'étoiles: " + item.getNbretoile());
-                    Label emplacementLabel = new Label("Emplacement: " + item.getEmplacement());
-                    Label avisLabel = new Label("Avis: " + item.getAvis());
-
-                    infoContainer.getChildren().addAll(nomLabel, nbreEtoilesLabel, emplacementLabel, avisLabel);
-
-                    // Créer le bouton "Afficher" pour chaque hôtel
-                    Button afficherButton = new Button("Afficher");
-                    afficherButton.setOnAction(event -> afficherDetailsHotel(item));
-
-                    // Ajouter la VBox et le bouton "Afficher" à la HBox
-                    container.getChildren().addAll(infoContainer, afficherButton);
-
-                    setGraphic(container);
-                }
-            }
-        });
+        hotelListView.setItems(hotels);
 
         // Définir le comportement lorsque la sélection de la liste change
         hotelListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue != null) {
-                // Mettre à jour les étiquettes avec les détails de l'hôtel sélectionné
-                nomid.setText(newValue.getNom());
-                nbretoilesid.setText(newValue.getNbretoile());
-                emplacementid.setText(newValue.getEmplacement());
-                avisid.setText(newValue.getAvis());
-                selectedHotel = newValue;
+                afficherDetailsHotel(newValue);
             }
         });
 
-        // Ajouter un écouteur sur le champ de recherche
-        rechercheTextField.textProperty().addListener((observable, oldValue, newValue) -> {
-            rechercherParNom(newValue);
-        });
+        // Afficher les statistiques
+        afficherStatistiques();
+
+        // Mettre en place la recherche par nom
+        setupRecherche();
     }
 
-    // Méthode pour afficher les détails de l'hôtel sélectionné
-    private void afficherDetailsHotel(hotel hotel) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/hotelAfficher.fxml"));
-            Parent root = loader.load();
 
-            // Passer l'hotel sélectionné au contrôleur de la vue hotelAfficher.fxml
-            hotelAfficher controller = loader.getController();
-            controller.setSelectedHotel(hotel);
-
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void afficherStatistiques() {
     }
-
-    // Méthode pour rechercher un hôtel par nom
-    private void rechercherParNom(String nom) {
-        List<hotel> hotels = hotelService.getAll();
-        List<hotel> filteredHotels = hotels.stream()
-                .filter(h -> h.getNom().toLowerCase().contains(nom.toLowerCase()))
-                .collect(Collectors.toList());
-        hotelListView.getItems().clear();
-        hotelListView.getItems().addAll(filteredHotels);
-    }
-
-    // Méthode pour trier les hôtels par nombre d'étoiles
     @FXML
     private void trierParEtoiles(ActionEvent actionEvent) {
         triAscendant = !triAscendant;
@@ -161,5 +93,151 @@ public class hotelList {
         }
         hotelListView.getItems().clear();
         hotelListView.getItems().addAll(hotels);
+    }
+
+
+    @FXML
+    private void calculerEtAfficherStatistiques() {
+        List<hotel> hotels = hotelService.getAll();
+        int totalHotels = hotels.size();
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+
+        // Parcourir les hôtels et compter le nombre d'hôtels pour chaque nombre d'étoiles
+        int[] nombreEtoiles = new int[6]; // Un tableau pour stocker le nombre d'hôtels pour chaque nombre d'étoiles (de 1 à 5)
+        for (hotel h : hotels) {
+            int nbreEtoiles = Integer.parseInt(h.getNbretoile());
+            if (nbreEtoiles >= 1 && nbreEtoiles <= 5) {
+                nombreEtoiles[nbreEtoiles]++;
+            }
+        }
+
+        // Ajouter les données au graphique camembert
+        for (int i = 1; i <= 5; i++) {
+            int hotelsPourCetteEtoile = nombreEtoiles[i];
+            double pourcentage = (double) hotelsPourCetteEtoile / totalHotels * 100;
+            pieChartData.add(new PieChart.Data(hotelsPourCetteEtoile + " hôtels - " + i + " étoiles", hotelsPourCetteEtoile));
+        }
+
+        // Afficher les statistiques dans le graphique camembert
+        PieChart pieChart = new PieChart(pieChartData);
+        pieChart.setTitle("Statistiques des hôtels par nombre d'étoiles");
+        pieChart.setLegendVisible(true);
+        pieChart.setLabelsVisible(true);
+
+        // Créer une nouvelle fenêtre pour afficher le graphique camembert
+        javafx.stage.Stage stage = new javafx.stage.Stage();
+        javafx.scene.Scene scene = new javafx.scene.Scene(pieChart);
+        stage.setScene(scene);
+        stage.setTitle("Statistiques des hôtels");
+        stage.show();
+    }
+
+
+    @FXML
+    private void setupRecherche() {
+        setupRecherche.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == null || newValue.isEmpty()) {
+                // Si le champ de recherche est vide, afficher tous les hôtels
+                hotelListView.getItems().setAll(hotelService.getAll());
+            } else {
+                // Filtrer les hôtels selon le texte de recherche
+                List<hotel> filteredHotels = hotelService.getAll().stream()
+                        .filter(h -> h.getNom().toLowerCase().contains(newValue.toLowerCase()))
+                        .collect(Collectors.toList());
+                hotelListView.getItems().setAll(filteredHotels);
+            }
+        });
+    }
+
+
+
+    private void afficherDetailsHotel(hotel hotel) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/hotelAfficher.fxml"));
+            Parent root = loader.load();
+
+            // Passer l'hotel sélectionné au contrôleur de la vue hotelAfficher.fxml
+            hotelAfficher controller = loader.getController();
+            controller.setSelectedHotel(hotel);
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Ajouter votre logique d'affichage des détails de l'hôtel ici
+    }
+
+    @FXML
+    private void supprimerHotel() {
+        hotel selectedHotel = hotelListView.getSelectionModel().getSelectedItem();
+        if (selectedHotel != null) {
+            Alert confirmationDialog = new Alert(Alert.AlertType.CONFIRMATION);
+            confirmationDialog.setTitle("Confirmation de suppression");
+            confirmationDialog.setHeaderText("Voulez-vous vraiment supprimer cet hôtel ?");
+            confirmationDialog.setContentText("Cette action est irréversible.");
+
+            confirmationDialog.showAndWait().ifPresent(response -> {
+                if (response == ButtonType.OK) {
+                    hotelService.delete(selectedHotel);
+                    afficherHotelSupprimeAvecSucces();
+                    hotels.remove(selectedHotel);
+                    effacerDetailsHotel();
+                }
+            });
+        } else {
+            afficherAucunHotelSelectionneAlert();
+        }
+    }
+
+    private void afficherHotelSupprimeAvecSucces() {
+        Alert successAlert = new Alert(Alert.AlertType.INFORMATION);
+        successAlert.setTitle("Suppression réussie");
+        successAlert.setHeaderText(null);
+        successAlert.setContentText("L'hôtel a été supprimé avec succès.");
+        successAlert.showAndWait();
+    }
+
+    private void afficherAucunHotelSelectionneAlert() {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Aucun hôtel sélectionné");
+        alert.setHeaderText(null);
+        alert.setContentText("Veuillez sélectionner un hôtel à supprimer.");
+        alert.showAndWait();
+    }
+
+    private void effacerDetailsHotel() {
+        nomLabel.setText("");
+        nbretoileLabel.setText("");
+        emplacementLabel.setText("");
+        avisLabel.setText("");
+    }
+
+    @FXML
+    private void updateHotel() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/hotelModifier.fxml"));
+            Parent root = loader.load();
+
+            // Obtenez une référence vers le contrôleur hotelModifier
+            hotelModifier controller = loader.getController();
+
+            // Obtenez l'hôtel sélectionné
+            hotel selectedHotel = hotelListView.getSelectionModel().getSelectedItem();
+
+            if (selectedHotel != null) {
+                // Passez l'hôtel sélectionné au contrôleur de modification
+                controller.initData(selectedHotel);
+
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.show();
+            } else {
+                afficherAucunHotelSelectionneAlert();
+            }
+        } catch (IOException e) {
+            e.printStackTrace(); // Gérer les erreurs de chargement de la page hotelModifier.fxml
+        }
     }
 }
